87dc97a154d624d0a5eead8f1cd922d785082161
Atoshi <atoshit@protonmail.com>
2024-09-25 04:59:48 +0200

    Full System

--- server.lua
+++ server.lua
@@ -2,7 +2,7 @@ ESX.RegisterServerCallback('atoshi:hasItem', function(source, cb)
     local xPlayer = ESX.GetPlayerFromId(source)
     if xPlayer then
         print("Oui")
-        print(xPlayer.hasItem(Config.itemName))
+        print(json.encode(xPlayer.hasItem(Config.itemName)))
         cb(xPlayer.hasItem(Config.itemName))
     end
 end)
