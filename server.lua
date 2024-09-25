1d3674188be28545f08face8a1f252be7b0a6a89
Atoshi <atoshit@protonmail.com>
2024-09-25 04:45:44 +0200

    Full System

--- server.lua
+++ server.lua
@@ -1,13 +1,11 @@
 RegisterCommand("draw:laser", function()
     local player = ESX.GetPlayerFromId(source)
-    local hasItem = (player.getInventoryItem(Config.itemName) > 0 and true or false)
 
     if player then
-        if hasItem then
+        if player.hasItem(Config.itemName) then
             TriggerClientEvent('atoshi:toggleLaser', source)
         else
             player.showNotification("Vous n'avez pas de laser")
         end
     end
 end)
-
